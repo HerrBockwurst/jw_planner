926ae48dc1b03dc72b1e7014b5758ba79070fda9
Kevin <chef@herrbockwurst.de>
2016-06-14 21:26:52 +0200

    s

--- sites/header.php
+++ sites/header.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<title><?php printTitle(true); ?></title>
+		<link rel="stylesheet" href="<?php printURL();?>/reset.css"></link>
 		<link rel="stylesheet" href="<?php printURL();?>/<?php getcss(); ?>"></link>
 	</head>
 	<body></body>

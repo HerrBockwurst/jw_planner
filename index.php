95ba237b6b4cdbd5c5d4232c5dc28ab85e0e6a7a
BuildTools <unconfigured@null.spigotmc.org>
2016-06-14 10:17:27 +0200

    ttest

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
 <?php
+session_start();
 require_once 'config.php';
 require_once 'oop/language.php';
 require_once 'libs/mysql.php';
 require_once 'oop/urlpath.php';
 
-var_dump($url->patharray);
+switch ($url[0]) {
+	case 'calendar':
+		echo "Kalender";
+		break;
+	case 'profile':
+		echo "Profil";
+		break;
+	default:
+		echo "Kalender";
+		break;
+}
 ?>

31279fa152a4ae08dd483fb5a33196d02e14dfce
Kevin <chef@herrbockwurst.de>
2016-06-14 19:23:54 +0200

    k

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once 'libs/functions.php';
 //printURL();
 
 $mysql = new mysqli($MYSQL_HOST, $MYSQL_USER, $MYSQL_PASSWORD, $MYSQL_DATABASE);
-//$result = $mysql->query("SELECT * FROM `config` WHERE `conf` = 'url' LIMIT 1");
+$result = $mysql->query("SELECT * FROM `config` WHERE `conf` = 'url' LIMIT 1");
 //$var_dump($result);
 //echo $mysql->error;
 

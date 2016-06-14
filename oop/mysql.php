81f6f5a71bffdb68a62451ef363b84b07bbf790a
Kevin <chef@herrbockwurst.de>
2016-06-14 19:13:06 +0200

    r

--- oop/mysql.php
+++ oop/mysql.php
@@ -16,7 +16,7 @@ class MySQL {
 	
 	function query($qry, $return = false) {
 		if($return):
-			$result = $this->mysql->query($qry) or die($this->mysql->error);
+			$result = $this->mysql->query($qry) or die("MySQL-Error: ".$this->mysql->error);
 			$aresult = $result->fetch_all();
 			$result->close();
 			return $aresult;

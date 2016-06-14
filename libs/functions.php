38a909b007c4f7aa0f47ebb733136b0ec64fb49c
Kevin <chef@herrbockwurst.de>
2016-06-14 20:46:14 +0200

    gf

--- libs/functions.php
+++ libs/functions.php
@@ -7,7 +7,8 @@ function printURL() {
 	global $mysql;
 	$url = $mysql->query("SELECT * FROM `config` WHERE `conf` = 'url' LIMIT 1", true);
 	$ssl = $mysql->query("SELECT * FROM `config` WHERE `conf` = 'ssl' LIMIT 1", true);
-	echo $ssl['value'];
+	$s2 = boolval($ssl['value']);
+	var_dump($s2);
 	if(boolval($ssl['value']))
 		echo "https://".$url['value'];
 	else 

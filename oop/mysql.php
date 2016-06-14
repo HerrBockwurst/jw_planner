0a363dd9ab6931c986d08161fcd1f1222ed065cb
Kevin <chef@herrbockwurst.de>
2016-06-14 19:50:34 +0200

    ds

--- oop/mysql.php
+++ oop/mysql.php
@@ -1,9 +1,13 @@
 <?php
 class MySQL {
 	protected $mysql;
+	private $MYSQL_HOST = $MYSQL_HOST;
+	private $MYSQL_USER = $MYSQL_USER;
+	private $MYSQL_PASSWORD = $MYSQL_PASSWORD;
+	private $MYSQL_DATABASE = $MYSQL_DATABASE;
 	
 	function __construct() {
-		$this->mysql = new mysqli($MYSQL_HOST, $MYSQL_USER, $MYSQL_PASSWORD, $MYSQL_DATABASE);
+		$this->mysql = new mysqli($this->MYSQL_HOST, $this->$MYSQL_USER, $this->$MYSQL_PASSWORD, $this->$MYSQL_DATABASE);
 		if ($this->mysql->connect_errno) {
 		    printf("Connect failed: %s\n", $this->mysql->connect_error);
 		    exit();

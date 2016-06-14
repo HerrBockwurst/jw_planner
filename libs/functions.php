1b74a5c076552284f1722631f8dab02d817577bf
BuildTools <unconfigured@null.spigotmc.org>
2016-06-14 12:26:15 +0200

    test

--- libs/functions.php
+++ libs/functions.php
@@ -5,7 +5,7 @@ function getTitle() {
 
 function printURL() {
 	global $mysql;
-	$result = $mysql->query("SELECT `url` FROM `config` LIMIT 1", true);
+	$result = $mysql->query("SELECT * FROM `config` WHERE `conf` = 'url' LIMIT 1", true);
 	echo $result[0];
 }
 ?>

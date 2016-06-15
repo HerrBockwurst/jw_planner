610a112d09955804a73799529ceee5baa4014fb5
BuildTools <unconfigured@null.spigotmc.org>
2016-06-15 11:22:43 +0200

    cs

--- libs/sysvars.php
+++ libs/sysvars.php
@@ -5,6 +5,8 @@ function initArray() {
 	global $mysql, $config;
 	$result = $mysql->query("SELECT * FROM `config`", true);
 	while($row = $result->fetch_assoc()):
+		if($row['value'] == 'true') $row['value'] = true;
+		if($row['value'] == 'false') $row['value'] = false;
 		$config[$row['conf']] = $row['value'];	
 	endwhile;
 	$mysql->free();

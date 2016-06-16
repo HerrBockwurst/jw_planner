5626ec86fa2f44302f1e6b52233ed6c93edb4ac7
BuildTools <unconfigured@null.spigotmc.org>
2016-06-16 12:06:31 +0200

    c

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $CONFIG['MYSQL_PASSWORD'] = '6TPhHjEYZQCmyEKz';
 $CONFIG['MYSQL_DATABASE'] = 'jwplanner';
 
 $CONFIG['ssl'] = false;
-$CONFIG['home'] = '127.0.0.1:90';
+$CONFIG['home'] = 'jw.herrbockwurst.de';
 $CONFIG['title'] = 'JW Planner';
 $CONFIG['version'] = 'PREALPHA 0.1';
 $CONFIG['version_count'] = '16w24';

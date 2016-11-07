8d55954a29fde29aca7882a5c64bc99d08c3a10c
Kevin <Kevin@Kevin-PC>
2016-11-07 20:17:30 +0100

    update

--- pages/calendar/areahead.php
+++ pages/calendar/areahead.php
@@ -10,17 +10,54 @@ if($mysql->fetchRow()->vsid != $user->vsid) exit;
 
 ?>
 
-<div id="monthswitch">
+<div id="monthswitch" style="margin-top: 10px;">
 	<div id="switch_pre" class="clickable">&#10096;</div>
 	<div id="switch_next" class="clickable">&#10097;</div>
-	<div id="switch_main" data-currMonth="<?php echo date('m', time())?>" data-currYear="<?php echo date('Y', time())?>">November 2016</div>
+	<div id="switch_main" data-currmonth="<?php echo date('m', time())?>" data-curryear="<?php echo date('Y', time())?>">&nbsp;</div>
 </div><br class="floatbreak" />
 <div id="c_cal">
 	<?php displayString('common loading')?>
 </div>
 
 <script>
-$(function() {
-	loadContent('<?php echo PROTO.HOME?>/load/calendar/getgrid', '#c_cal');
+function loadCal() {
+
+	var months = ["", <?php 
+		for($i = 1; $i <= 12; $i++) {
+			echo "\"".getString('months '.strtolower(date('M', strtotime('1.'.$i.'.2000'))))."\"";
+			if($i != 12) echo ", ";
+		}		
+	?>];
+	
+	$('#switch_main').text(months[$('#switch_main').attr('data-currmonth')] + " " + $('#switch_main').attr('data-curryear'));
+	
+	
+	loadContent('<?php echo PROTO.HOME?>/load/calendar/getgrid', '#c_cal',
+			{cid: <?php echo $_POST['cid']?>, month: $('#switch_main').attr('data-currmonth'), year:  $('#switch_main').attr('data-curryear')});	
+}
+
+$(loadCal);
+
+$('#monthswitch').children('div').click(function() {
+	if(!$(this).hasClass('clickable')) return;
+	
+	var mainswitch = $('#switch_main');
+	var month = parseInt(mainswitch.attr('data-currmonth'));
+	var year = parseInt(mainswitch.attr('data-curryear'));
+
+	month = $(this).attr('id') == "switch_pre" ? month - 1 : month + 1;
+
+	if(month < 1) {
+		month = 12;
+		year = year - 1;
+	} else if(month > 12) {
+		month = 1;
+		year = year + 1;
+	}
+
+	mainswitch.attr('data-currmonth', month);
+	mainswitch.attr('data-curryear', year);
+
+	loadCal();
 });
 </script>
--- pages/calendar/grid.php
+++ pages/calendar/grid.php
@@ -1,3 +1,95 @@
 <?php
+if(!isset($_POST)) returnErrorJSON('No Data given!');
+
+global $mysql, $user;
+
+$mysql->where('calendar.cid', $_POST['cid']);
+$mysql->join(array("calendar" => 'vsid', "versammlungen" => "vsid"));
+$mysql->select("calendar", array('versammlungen.vsid' => "vsid"), 1);
+
+if($mysql->countResult() != 1) exit;
+if($mysql->fetchRow()->vsid != $user->vsid) exit;
+
+
+$daysOfMonth = cal_days_in_month(CAL_GREGORIAN, $_POST['month'], $_POST['year']);
+$month = intval($_POST['month']);
+$year = intval($_POST['year']);
+$pointerCol = 1;
+$currDay = 1;
+
+$startStamp = strtotime("1.".$month.".".$year);
+$endStamp = strtotime($daysOfMonth.".".$month.".".$year) + (60*60*24) - 1;
+
+/*
+ * Termine auslesen
+ */
+
+$daysWithPosts = array();
+
+$mysql->where('start', $startStamp, '>=');
+$mysql->where('end', $endStamp, '<=');
+$mysql->where('cid', $_POST['cid']);
+$mysql->select('posts', array('start'));
+
+$result = $mysql->fetchAll();
+foreach($result AS $currPost) $daysWithPosts[date('j', $currPost['start'])] = 1;
+
 ?>
-hi
+<div id="c_backbutton" class="clickable">&#10096;</div>
+<?php 
+
+
+/*
+ * Tabelle schreiben
+ */
+
+echo "<table id=\"c_table\">";
+echo "<tr>
+		<th class=\"shader\">".substr(getString('common monday'),0, 2)."</th>
+		<th class=\"shader\">".substr(getString('common tuesday'),0, 2)."</th>
+		<th class=\"shader\">".substr(getString('common wednesday'),0, 2)."</th>
+		<th class=\"shader\">".substr(getString('common thursday'),0, 2)."</th>
+		<th class=\"shader\">".substr(getString('common friday'),0, 2)."</th>
+		<th class=\"shader\">".substr(getString('common saturday'),0, 2)."</th>
+		<th class=\"shader\">".substr(getString('common sunday'),0, 2)."</th>
+</tr>";
+
+while($pointerCol < 8) {
+	while($currDay <= $daysOfMonth) {
+		if($pointerCol == 1) echo "<tr>";
+		
+		$weekdayOfcurrDay = date("N", strtotime($currDay.".".$month.".".$year)) == 0 ? 7 : date("N", strtotime($currDay.".".$month.".".$year));		
+		
+		if($weekdayOfcurrDay == $pointerCol) {
+			$highlight = isset($daysWithPosts[$currDay]) ? "highlight" : ""; 
+			echo "<td class=\"clickable $highlight\" data-date=\"$currDay\">".$currDay."</td>";
+			$currDay++;
+		}
+		else echo "<td class=\"shader\"></td>";
+		
+		$pointerCol++;
+		if($pointerCol > 7) {
+			echo "</tr>";
+			$pointerCol = 1;
+		}
+	}
+	
+	echo "<td class=\"shader\"></td>";
+	$pointerCol++;
+	if($pointerCol > 7) {
+		echo "</tr>";
+	}
+}
+
+echo "</table>";
+?>
+<br class="floatbreak" />
+<script>
+$('td.clickable').click(function() {
+
+	var attr = {height: ($('#c_table').height() - 22) +  'px', "line-height": ($('#c_table').height()) +  'px'}
+	
+	$('#c_backbutton').css(attr).show('slide', {direction: 'left'}, 1000); 
+	$('#c_table').hide('slide', {direction: 'left'}, 1000);
+});
+</script>

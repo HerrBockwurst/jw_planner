cc528cd0fefa3b527c4074f4e12a8320a35f689f
BuildTools <unconfigured@null.spigotmc.org>
2016-11-27 19:07:03 +0100

    update

--- pages/donate/index.php
+++ pages/donate/index.php
@@ -2,9 +2,13 @@
 	<div class="headline"><?php displayString('menu donate')?></div>
 	<p><?php displayString('donate info')?></p>
 	<p><?php displayString('donate cost')?></p>
-	<p><?php displayString('donate overflow')?></p>
 	<p><?php displayString('donate methods')?></p>
-	<div style="text-align: center">
+	<p style="float: left; margin-left: 20px;">
+	Kevin Wagner<br />
+	IBAN: DE53 5001 0517 5410 5464 30<br />
+	BIC: INGDDEFFXXX<br />
+	ING-Diba</p>
+	<div style="float:right; margin-right: 20px;">
 		<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_blank">
 			<input type="hidden" name="cmd" value="_s-xclick">
 			<input type="hidden" name="hosted_button_id" value="G9QRJ8BQ4JZNJ">
@@ -12,4 +16,5 @@
 			<img alt="" border="0" src="https://www.paypalobjects.com/de_DE/i/scr/pixel.gif" width="1" height="1">
 		</form>
 	</div>
+	<br class="floatbreak" />
 </div>

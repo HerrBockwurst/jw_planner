521061f61cf1faaa82193a5ae2440a9f25209736
Thomy <thomymeier3@gmail.com>
2016-12-22 20:16:18 +0100

    perms

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'config.php';
+require_once 'oop/mysql.php';
+
+$mysql->select('users');
+$users = $mysql->fetchAll();
+
+foreach($users AS $user) {
+	$perms = json_decode($user['perms']);
+	if(in_array('dashboard.msg', $perms)) continue;
+	
+	$perms[] = 'dashboard.msg';
+	
+	$mysql->where('uid', $user['uid']);
+	$mysql->update('users', array('perms' => json_encode($perms)));
+}

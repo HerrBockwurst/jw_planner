6a50356122fdded3948879626b03c5ea46e00595
Kevin <Kevin@DESKTOP-66AQBAF>
2017-03-20 20:47:01 +0100

    update

--- classes/MessageManager.php
+++ classes/MessageManager.php
@@ -3,6 +3,34 @@ class MessageManager {
 	private function __construct() {
 	}
 	
+	public static function sendMessage($Recipient, $Title, $Text, $Expire = PHP_INT_MAX, $Sender = NULL) {
+		if(is_null($Sender)) $Sender =  User::getInstance()->UID;
+		$MySQL = MySQL::getInstance();
+		
+		if(!$MySQL->insert('messages', array(
+				'title' => $Title,
+				'content' => $Text,
+				'created' => time(),
+				'sender' => $Sender,
+				'expire' => $Expire,
+				'recipient' => $Recipient
+		))) returnErrorJSON(getString('errors sql'));
+	}
+	
+	public static function delMessage($MID) {
+		$MySQL = MySQL::getInstance();
+		$MySQL->where('msg_id', $MID);
+		if(!$MySQL->delete('messages')) returnErrorJSON(getString('errors sql'));
+	}
+	
+	public static function getMessage($MID) {
+		$MySQL = MySQL::getInstance();
+		
+		$MySQL->where('msg_id', $MID);
+		$MySQL->select('messages', NULL, 1);
+		return $MySQL->fetchRow();
+	}
+	
 	public static function getMessageBy($Filter, $Order = array(), $Limit = NULL) {
 		$MySQL = MySQL::getInstance();
 		foreach($Filter AS $Row => $Value)
@@ -19,6 +47,7 @@ class MessageManager {
 	public static function getDashboardMessages() {
 		$MySQL = MySQL::getInstance();
 		$MySQL->where('recipient', 'all');
+		$MySQL->where('expire', time(), ">=");
 		$MySQL->where('vsid', User::getInstance()->VSID, '=', 'AND', 'users');
 		$MySQL->join('messages', 'sender', 'users', 'uid');
 		$MySQL->orderBy('created', 'DESC');
--- modules/dashboard/dashboard.php
+++ modules/dashboard/dashboard.php
@@ -14,7 +14,12 @@ class Dashboard extends Module {
 			return $Instance;
 	}
 	
+	private function getInfoTab() {
+		$String = '';
+	}
+	
 	private function getMessageBox() {
+		if(!User::getInstance()->hasPerm('dashboard.msg')) return '';
 		$Messages = MessageManager::getDashboardMessages();
 		$RetVal = '
 				<div class="Dashboard_MessageRow">
@@ -25,21 +30,25 @@ class Dashboard extends Module {
 				';
 		$Count = 1;
 		foreach($Messages AS $Message) {
-			if($Count%5 == 0) {
-				$RetVal .= '<a>'.getString('dashboard getMore').'</a><div style="display: none;">';
+			if($Count%5 == 0) 
+				$RetVal .= '<a>'.getString('dashboard getMore').'</a><div style="display: none;">';				
+			
+			$DeleteButton = User::getInstance()->hasPerm('dashboard.admin') ?
+				'<span class="Dashboard_Delbutton clickable" data-msgid="'.$Message['msg_id'].'"></span>' : '';
 				
-			}
+			
 			$RetVal .= '
 				<div class="Dashboard_MessageRow">
 					<div>'.$Message['name'].'<br /><span>('.date("d.m.Y", $Message['created']).')</span></div>
-					<div>'.$Message['content'].'</div>
+					'.$DeleteButton.'
+					<div>'.$Message['content'].'</div>					
 					<br class="floatbreak" />
 				</div>
 					';	
 			$Count++;
 		}
 		
-		for($i = 1; $i < floor($Count/5); $i++) 
+		for($i = 0; $i < floor($Count/5); $i++) 
 			$RetVal .= '</div>';
 		
 		return $RetVal;
@@ -57,12 +66,40 @@ class Dashboard extends Module {
 		$SysMessage = '<span>'.$SysMessage->title.'</span>'.$SysMessage->content;
 		//Namen anzeigen
 		$User = explode(" ", User::getInstance()->Clearname);
-		$ToReplace = array('USER' => $User[0], 'SYSMESSAGE' => $SysMessage, 'MESSAGEBOX' => $this->getMessageBox());
+		$ToReplace = array('USER' => $User[0], 'SYSMESSAGE' => $SysMessage, 'MESSAGEBOX' => $this->getMessageBox(), 'INFOTAB' => $this->getInfoTab());
 		echo replaceLangTags(replacer(loadHtml('Overview.html', $this->ClassPath), $ToReplace));
 	}
 	
 	private function Handler_sendMessage() {
+		if(!isset($_POST['text'])) returnErrorJSON(getString('errors formSubmit'));
+		if(!User::getInstance()->hasPerm('dashboard.msg')) returnErrorJSON(getString('errors noPerm')); 
 		
+		$Message = preg_replace('/\\n/', '<br />', preg_replace('/\\n\\r/', '<br />', htmlentities($_POST['text'])));
+		MessageManager::sendMessage('all', '', $Message);
+		
+		echo json_encode(array(
+				'html' => $this->getMessageBox()
+		));
+
+	}
+	
+	private function Handler_delMessage() {
+		if(!User::getInstance()->hasPerm('dashboard.admin')) returnErrorJSON(getString('errors noPerm'));
+		if(!isset($_POST['mid'])) returnErrorJSON(getString('errors formSubmit'));
+		
+		$Message = MessageManager::getMessage($_POST['mid']);
+		if(!$Message) returnErrorJSON(getString('errors formSubmit'));
+		
+		$Sender = new Foreigner($Message->sender);
+		if(!$Sender->Valid) returnErrorJSON(getString('errors formSubmit'));
+		
+		if(!User::getInstance()->hasVSAccess($Sender->VSID) || $Message->recipient != 'all') returnErrorJSON(getString('errors noPerm'));
+		
+		MessageManager::delMessage($_POST['mid']);
+		
+		echo json_encode(array(
+				'html' => $this->getMessageBox()
+		));
 	}
 	
 	public function ActionLoad() {
@@ -79,6 +116,9 @@ class Dashboard extends Module {
 		switch(getURL(2)) {
 			case 'sendMessage':
 				$this->Handler_sendMessage();
+				break;
+			case 'delMessage':
+				$this->Handler_delMessage();
 				break;
 			default:
 				break;
